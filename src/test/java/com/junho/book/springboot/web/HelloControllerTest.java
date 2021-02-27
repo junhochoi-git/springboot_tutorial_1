70a961e323a266f72b601a9b25647a7fa47f63ef
junhochoi-git <ajchoi0928@gmail.com>
2021-02-27 16:49:32 +0900

    스프링부트 2.4.1 로 버전업 gradle 6.7.1 업 JUnit 5 적용

---
+++ src/test/java/com/junho/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package com.junho.book.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@RunWith(SpringRunner.class) // (1)
+@ExtendWith(SpringExtension.class) //(1)-2 JUNIT 5 으로 버전업
+@WebMvcTest(controllers = HelloController.class) // (2)
+public class HelloControllerTest {
+
+    @Autowired // (3)
+    private MockMvc mvc; // (4)
+
+    @WithMockUser(roles="USER")
+    @Test
+    public void helloTest() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+
+    }
+}
